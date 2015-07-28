e8f74a4777b0134faa1d43f7bfcf158ef15524dc
davidmarcbrown <david@davidmarcbrown.com>
2015-07-28 18:48:14 -0400

    Add alarm functionality

--- SimpleWP8AlarmClock/MainPage.xaml.cs
+++ SimpleWP8AlarmClock/MainPage.xaml.cs
@@ -35,8 +35,12 @@ namespace SimpleWP8AlarmClock
         {
             Alarm existingAlarm = (Alarm) ScheduledActionService.Find(ALARM_NAME);
 
-            if (existingAlarm != null && existingAlarm.IsEnabled == true)
+            if (existingAlarm != null && existingAlarm.BeginTime > DateTime.Now)
             {
+                DateTime beginTime = existingAlarm.BeginTime;
+                String alarmInfo = String.Format("Alarm will sound at {0:t}.", beginTime);
+
+                alarmInfo_textBlock.Text = alarmInfo;
                 alarmInfo_panel.Visibility = Visibility.Visible;
                 setAlarm_panel.Visibility = Visibility.Collapsed;
                 ((ApplicationBarIconButton)ApplicationBar.Buttons[0]).IsEnabled = false;
@@ -44,6 +48,10 @@ namespace SimpleWP8AlarmClock
             }
             else
             {
+                if (existingAlarm != null)
+                {
+                    alarmTimePicker.Value = existingAlarm.BeginTime;
+                }
                 alarmInfo_panel.Visibility = Visibility.Collapsed;
                 setAlarm_panel.Visibility = Visibility.Visible;
                 ((ApplicationBarIconButton)ApplicationBar.Buttons[0]).IsEnabled = true;
@@ -52,18 +60,66 @@ namespace SimpleWP8AlarmClock
         }
 
         private void ApplicationBarDeleteButton_Click(object sender, EventArgs e)
-        {
+        {           
+            Alarm existingAlarm = (Alarm) ScheduledActionService.Find(ALARM_NAME);
 
+            if (existingAlarm != null)
+            {
+                ScheduledActionService.Remove(ALARM_NAME);
+            }
+            updateUI();
         }
 
         private void ApplicationBarSaveButton_Click(object sender, EventArgs e)
         {
+            DateTime beginTime = (DateTime)alarmTimePicker.Value;
 
+            if (beginTime < DateTime.Now)
+                beginTime = beginTime.AddDays(1);
+            DateTime expirationTime = beginTime.AddSeconds(1);
+
+            RecurrenceInterval recurrence = RecurrenceInterval.None;
+
+            Alarm alarm = new Alarm(ALARM_NAME);
+            alarm.Content = ALARM_CONTENT;
+            alarm.Sound = new Uri("/Assets/Sounds/Alarm-06.wma", UriKind.Relative);
+            alarm.BeginTime = beginTime;
+            alarm.ExpirationTime = expirationTime;
+            alarm.RecurrenceType = recurrence;
+
+            if (ScheduledActionService.Find(ALARM_NAME) != null)
+            {
+                ScheduledActionService.Remove(ALARM_NAME);
+            }
+            ScheduledActionService.Add(alarm);
+
+            updateUI();
         }
 
-        private void alarmTimePicker_ValueChanged(object sender, DateTimeValueChangedEventArgs e)
+        private void alarmInfo_panel_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
+            Alarm existingAlarm = (Alarm) ScheduledActionService.Find(ALARM_NAME);
 
+            if (existingAlarm != null)
+            {
+                String alarmInfo;
+                TimeSpan remainingTime = existingAlarm.BeginTime.Subtract(DateTime.Now);
+
+                if (remainingTime.Hours > 0)
+                {
+                    alarmInfo = String.Format("Alarm will sound in {0} hours and {1} minutes.", remainingTime.Hours, remainingTime.Minutes);
+                }
+                else if (remainingTime.Minutes > 0)
+                {
+                    alarmInfo = String.Format("Alarm will sound in {0} minutes.", remainingTime.Minutes);
+                }
+                else
+                {
+                    alarmInfo = "Alarm will sound in less than one minute.";
+                }
+
+                MessageBox.Show(alarmInfo);
+            }
         }
     }
 }
